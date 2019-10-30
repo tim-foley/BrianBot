cebefcdf1093f912a42270651104d6186797a320
Ethan <edsalisb90@gmail.com>
2019-10-29 20:35:48 -0700

    Brian bot 1.0

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
-
-const bradOnEachTeam = {
-    'TDJ8STMFE': 'UJT4QPQ90',
-    'T2TJSK16K': 'UGACR0GE4'
-};
+const allowedChannels = ['GNTRBT3QA',  // bot squad
+'GECRA8741', // it devs
+'GED6NQTNX', // it
+'C2TV7N10C', // QA
+'GMQJ70QGG', // classic wow
+'CECEET1H8', // photoshop
+'CMVMYBD1S'  // mean team
+] 
 
 app.use(bodyParser());
 app.post('/', function (req, res) {
@@ -23,18 +26,53 @@ app.post('/', function (req, res) {
     const event = body ? body.event : null;
     console.log('BODY', body)
     
+    if (allowedChannels.indexOf(event.channel) > -1){
+        res.status(200).send();
+    }
+    else{
+        maybeSendALol(body.token, event, () => {
+            res.status(200).send()
+        })
+    }
 
-    res.status(200).send();
+    
 
 })
 
-function messageAsBrad(token, event, cb) {
+function maybeSendALol(token, event, cb){
+    const RARE_MESSAGES = ['Shut Up Spencer'];
+    const LMAO_MESSAGES = ['haha', 'LOL'];
+    const LOL_MESSAGES = ['lol', 'haha']
+    let listToUse;
+    if (determineRandomness(1, 10000000)){
+        listToUse = RARE_MESSAGES
+    }
+    else if (determineRandomness(1, 1000)){
+        listToUse = LMAO_MESSAGES;
+    }
+    else if (determineRandomness(1, 5)){
+        listToUse = LOL_MESSAGES;
+    }
+    else{
+        //noop
+        cb();
+        return;
+    }
+    let message = listToUse[Math.floor(Math.random() * listToUse.length)];
+    return sendLol(token, event, message, cb);
+}
+
+function determineRandomness(expectedVal, outcomes){
+    return Math.floor(Math.random() * outcomes) === expectedVal;
+}
+
+function sendLol(token, event, message, cb) {
     if (typeof event !== 'object') return cb();
 
 
     const body = JSON.stringify({
         channel: event.channel,
-        text: `${determineMessage(event)} <@${event.user}>!`,
+        text: `${message}`,
         thread_ts: event.thread_ts || undefined
     })
 
